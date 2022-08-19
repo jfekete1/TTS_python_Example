23b26d35f020239e5f4840f3f46adaca09fb05cf
jfekete1 <fekete.jozsef@protonmail.com>
2022-08-19 09:25:28 +0200

    removed comments

--- tts_client.py
+++ tts_client.py
@@ -2,7 +2,6 @@ import requests
 import json
 import sys, base64
 
-#url = 'http://localhost:5000/speech'
 url = 'http://localhost:8080/speech'
 
 payload = {
@@ -17,7 +16,6 @@ headers = {
 para = {'speech': "\"ez egy árvíztűrő tükörfúrógép\""}
 
 r = requests.get(url, headers=headers, params=para)
-#r = requests.post(url, json = payload)
 encoded_mp3_dict = json.loads(r.text)
 encoded_mp3 = encoded_mp3_dict['speech']
 
@@ -27,8 +25,6 @@ print(type(encoded_mp3))
 
 
 b_str = encoded_mp3.encode()
-# print("UTF-8 encoded: ")
-# print(b_str)
 c = base64.b64decode(b_str)
 
 try:
